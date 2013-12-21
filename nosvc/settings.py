cb78803efad452c166d0ba66abc13ab39f39e6b0
Henrique Bastos <henrique@bastos.net>
2013-12-21 18:05:05 -0200

    Install django-suit

--- nosvc/settings.py
+++ nosvc/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = (
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
