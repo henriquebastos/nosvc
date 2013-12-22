ca42d2af4dce9a1d91bef087d123a925a0099a20
Henrique Bastos <henrique@bastos.net>
2013-12-22 02:29:51 -0200

    Add simple Meeting model

--- nosvc/core/models.py
+++ nosvc/core/models.py
@@ -1,4 +1,32 @@
 # coding: utf-8
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Meeting(TimeStampedModel):
+    # user
+    # backers
+    # rewards
+    # updates
+    # dynamic fields
+    title = models.CharField(max_length=255)
+    headline = models.TextField(max_length=140)
+    about = models.TextField()
+    location = models.TextField()
+    when = models.CharField(max_length=255)
+    expiration = models.DateTimeField()
+
+    video_url = models.URLField(null=True)
+    image_url = models.URLField(null=True)
+
+    visible = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=False)
+    finished = models.BooleanField(default=False)
+
+    about_host = models.TextField(null=True)
+
+    min_guests = models.PositiveIntegerField(null=True)
+    max_guests = models.PositiveIntegerField(null=True)
+
+    def __unicode__(self):
+        return self.title
---
+++ nosvc/core/tests/test_models_meeting.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from django.test import TestCase
+from datetime import datetime
+from nosvc.core.models import Meeting
+
+
+class MeetingModelTest(TestCase):
+    def setUp(self):
+        self.obj = Meeting.objects.create(
+            title='Meeting Title',
+            headline='Meeting headline.',
+            about='About the meeting.',
+            location='Meeting address.',
+            when='Quarta, 11 de Dezembro de 2013 das 19h às 22h.',
+            expiration='2014-01-01'
+        )
+
+    def test_create(self):
+        self.assertEqual(1, self.obj.pk)
+
+    def test_unicode(self):
+        self.assertEqual('Meeting Title', unicode(self.obj))
+
+    def test_timestamps(self):
+        self.assertIsInstance(self.obj.created, datetime)
+        self.assertIsInstance(self.obj.modified, datetime)
+
+    def test_flags_default_value(self):
+        self.assertFalse(self.obj.visible)
+        self.assertFalse(self.obj.confirmed)
+        self.assertFalse(self.obj.finished)
