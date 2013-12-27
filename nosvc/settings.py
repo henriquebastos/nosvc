97969bcc17c25ea391080df8cb0398b1c33d98a2
Henrique Bastos <henrique@bastos.net>
2013-12-27 21:49:22 -0200

    Use django-floppymodelforms

--- nosvc/settings.py
+++ nosvc/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'nosvc.floppyfields',
     'floppyforms',
+    'floppymodelforms',
     'nosvc.core',
 )
 
