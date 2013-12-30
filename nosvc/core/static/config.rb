4e788e7597ebdf557914bc39fa347bd08dfac284
Henrique Bastos <henrique@bastos.net>
2013-12-30 18:32:46 -0200

    Sass experiments with content and header.

---
+++ nosvc/core/static/config.rb
@@ -0,0 +1,12 @@
+# Compass configuration file.
+
+http_path = "/static/"
+css_dir = "stylesheets"
+sass_dir = "stylesheets"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+asset_cache_buster :none
