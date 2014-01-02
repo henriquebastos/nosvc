bb14e7dabf86fa93373e40c43a43d2769cc5db6e
Henrique Bastos <henrique@bastos.net>
2014-01-02 03:54:06 -0200

    Add url to render any template during debug mode.

--- nosvc/urls.py
+++ nosvc/urls.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 from nosvc.core.views import MeetingCreateView
 
 
@@ -12,3 +13,8 @@ urlpatterns = patterns('',
     url(r'^(?P<slug>[\w-]+)/$', 'nosvc.core.views.meeting_detail', name='meeting_detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^(?P<template_name>.+)$', 'django.shortcuts.render'),
+    )
